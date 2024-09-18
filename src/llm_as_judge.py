44d1700fb34a883e52918e397695c0204ce0386a
Ankush Garg <github@ankushgarg.com>
2024-09-18 15:54:58 -0500

    Refactor LLM as a judge

--- src/llm_as_judge.py
+++ src/llm_as_judge.py
@@ -1,4 +1,3 @@
-from .prompt import construct_prompt
 import os
 import json
 import uuid
@@ -9,15 +8,10 @@ from langchain_anthropic import ChatAnthropic
 from langgraph.graph import StateGraph, START, END
 from typing import Annotated, TypedDict, Sequence
 from langchain_core.messages import BaseMessage
-import yaml
+from .prompt import construct_prompt
 
-config = yaml.safe_load(open('config.yml'))
 
 load_dotenv()
-
-llm = ChatAnthropic(
-    model=config.get("MODEL_NAME"), api_key=os.getenv("ANTHROPIC_API_KEY")
-)
 
 
 class State(TypedDict):
@@ -28,83 +22,93 @@ class State(TypedDict):
     validation: Annotated[Sequence[BaseMessage], operator.add]  # Binary 1 or 0
     reason: Annotated[Sequence[BaseMessage], operator.add]
 
+class JudgeLLM:
+    def __init__(self):
+        self.llm = None
 
-def validate_response(state: State):
-    input = state["input"][-1]
-    output = state["output"][-1]
+    def validate_response(self, state: State):
+        input = state["input"][-1]
+        output = state["output"][-1]
 
-    # Input: Input to the LLM
-    # Output: LLM response
-    constructed_prompt = construct_prompt(
-        input, output, validation_key="validation", reason_key="reason"
-    )
+        # Input: Input to the LLM
+        # Output: LLM response
+        constructed_prompt = construct_prompt(
+            input, output, validation_key="validation", reason_key="reason"
+        )
 
-    response = llm.invoke(constructed_prompt)
-    content = json.loads(response.content)
+        response = self.llm.invoke(constructed_prompt)
+        content = json.loads(response.content)
 
-    return {"validation": [content.get("validation")], "reason": [content["reason"]]}
+        return {"validation": [content.get("validation")], "reason": [content["reason"]]}
 
+    def construct_validator_graph(self, State):
+        graph = StateGraph(State)
+        graph.add_node("validate_response", self.validate_response)
 
-def construct_validator_graph(State):
-    graph = StateGraph(State)
-    graph.add_node("validate_response", validate_response)
+        graph.add_edge(START, "validate_response")
+        graph.add_edge("validate_response", END)
 
-    graph.add_edge(START, "validate_response")
-    graph.add_edge("validate_response", END)
+        graph = graph.compile()
+        return graph
+    
+    def convert_llm_responses_to_dataframe(self, llm_evals: dict) -> pd.DataFrame:
+        dataframe = pd.DataFrame()
 
-    graph = graph.compile()
-    return graph
+        for batch_id, evaluation in llm_evals.items():
+            validations = {f"{r['node_name'][0]}": r["validation"][0] for r in evaluation}
+            reasons = {f"{r['node_name'][0]}_reason": r["reason"] for r in evaluation}
+            inputs = {f"input_{idx+1}": r["input"] for idx, r in enumerate(evaluation)}
+            outputs = {
+                f"output_{idx+1}": r["output"][0] for idx, r in enumerate(evaluation)
+            }
 
+            row_data = {**inputs, **outputs, **validations, **reasons}
+            row_data["batch_id"] = batch_id
 
-def convert_llm_responses_to_dataframe(llm_evals: dict) -> pd.DataFrame:
-    dataframe = pd.DataFrame()
+            validations_df = pd.DataFrame([row_data])
+            dataframe = pd.concat([dataframe, validations_df], ignore_index=True)
 
-    for batch_id, evaluation in llm_evals.items():
-        validations = {f"{r['node_name'][0]}": r["validation"][0] for r in evaluation}
-        reasons = {f"{r['node_name'][0]}_reason": r["reason"] for r in evaluation}
-        inputs = {f"input_{idx+1}": r["input"] for idx, r in enumerate(evaluation)}
-        outputs = {
-            f"output_{idx+1}": r["output"][0] for idx, r in enumerate(evaluation)
-        }
+        return dataframe
+    
+    def run(
+            self,
+            llm_name: str,
+            dataframe: pd.DataFrame, node_input_output_mappings: dict[tuple]
+        ) -> pd.DataFrame:
+        self.llm = ChatAnthropic(model=llm_name, api_key=os.getenv("ANTHROPIC_API_KEY"))
+        graph = self.construct_validator_graph(State)
 
-        row_data = {**inputs, **outputs, **validations, **reasons}
-        row_data["batch_id"] = batch_id
+        # TODO implement Async code for this
+        llm_evals = {}
+        for _, row in dataframe.iterrows():
+            json_data = row.to_dict()
+            batch_id = json_data.get("batch_id", uuid.uuid4().hex)
 
-        validations_df = pd.DataFrame([row_data])
-        dataframe = pd.concat([dataframe, validations_df], ignore_index=True)
+            input_output_evaluation = []
+            for node_name, mappings in node_input_output_mappings.items():
+                prompt_key, response_key = mappings
+                prompt_text = json_data.get(prompt_key, None)
+                output_text = json_data.get(response_key, None)
 
-    return dataframe
+                evaluation_response = graph.invoke(
+                    {
+                        "input": [prompt_text],
+                        "output": [output_text],
+                        "node_name": [node_name],
+                    }
+                )
+                
+                input_output_evaluation.append(evaluation_response)
 
+            llm_evals[batch_id] = input_output_evaluation
 
-def run_validation_using_LLM(
-    dataframe: pd.DataFrame, node_input_output_mappings: dict[tuple]
-) -> pd.DataFrame:
-    graph = construct_validator_graph(State)
+        dataframe = self.convert_llm_responses_to_dataframe(llm_evals=llm_evals)
+        return dataframe
 
-    # TODO implement Async code for this
-    llm_evals = {}
-    for _, row in dataframe.iterrows():
-        json_data = row.to_dict()
-        batch_id = json_data.get("batch_id", uuid.uuid4().hex)
-
-        input_output_evaluation = []
-        for node_name, mappings in node_input_output_mappings.items():
-            prompt_key, response_key = mappings
-            prompt_text = json_data.get(prompt_key, None)
-            output_text = json_data.get(response_key, None)
-
-            evaluation_response = graph.invoke(
-                {
-                    "input": [prompt_text],
-                    "output": [output_text],
-                    "node_name": [node_name],
-                }
-            )
-
-            input_output_evaluation.append(evaluation_response)
-
-        llm_evals[batch_id] = input_output_evaluation
-
-    dataframe = convert_llm_responses_to_dataframe(llm_evals=llm_evals)
-
-    return dataframe
+def run_validations_using_llm(llm_name: str, dataframe=pd.DataFrame, node_input_output_mappings=dict):
+    llm_judge = JudgeLLM()
+    return llm_judge.run(
+        llm_name=llm_name,
+        dataframe=dataframe, 
+        node_input_output_mappings=node_input_output_mappings
+        )
