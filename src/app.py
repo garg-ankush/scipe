9ba6ab8d2dfc040c9b01aa3707a4e0a7153d22bc
Ankush Garg <github@ankushgarg.com>
2024-09-25 13:20:01 -0500

    Fix bug

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from .algorithm import find_problematic_node
 
 
 class LLMEvaluator:
-    def __init__(self, config_path: str, responses: pd.DataFrame, application_dag: defaultdict):
+    def __init__(self, config_path: str, responses: pd.DataFrame, graph: defaultdict):
         """LLM Evaluator for running LLM-based evaluations on application data
 
         Args:
@@ -19,7 +19,7 @@ class LLMEvaluator:
         self.config_dir = os.path.dirname(config_path)
         self.config = self._load_config(config_path)
         self.responses = responses
-        self.application_dag = application_dag
+        self.graph = graph
         self._validations = None
 
     def _load_config(self, config_path: str) -> Dict[str, Any]:
@@ -46,12 +46,13 @@ class LLMEvaluator:
         llm_validations = run_validations(
                 model_name=self.config["MODEL_NAME"],
                 dataframe=self.responses,
-                node_input_output_mappings=self.config["node_input_output_mappings"]
-                )
+                node_input_output_mappings=self.config["node_input_output_mappings"])
+
+        self.llm_validations = llm_validations
+
         # Save these down in case the user wants to use them again
         llm_validations.to_csv(f"{self.config_dir}/{self.config["PATH_TO_SAVE_VALIDATIONS"]}", index=None)
 
-        self.llm_validations = llm_validations
         return self
     
     def find_problematic_node(self) -> Any:
@@ -61,9 +62,12 @@ class LLMEvaluator:
         Returns:
             Any: Prints out the result about which node to work on.
         """
-        if self.llm_validations is None or self.application_dag is None:
-            raise ValueError("Validations and application graph must be loaded before finding the problematic node.")
+        if self.graph is None:
+            raise ValueError("Graph must be loaded before looking for the problematic node.")
+
+        if self.llm_validations is None:
+            raise ValueError("LLM Validations must be run looking for the problematic node.")
         
-        return find_problematic_node(data=self.llm_validations, dag=self.application_dag)
+        return find_problematic_node(data=self.llm_validations, dag=self.graph)
         
 
