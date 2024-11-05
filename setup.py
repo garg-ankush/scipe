38cd76a90ca7f946a94ac17ff26889d4a346057a
Ankush Garg <github@ankushgarg.com>
2024-11-05 13:28:41 -0600

    Update minor version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="scipe",
-    version="1.0.0",
+    version="1.0.1",
     package_dir={"scipe": "src"},
     packages=["scipe"],
     author="Ankush Garg",
